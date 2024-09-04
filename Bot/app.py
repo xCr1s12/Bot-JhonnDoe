c91a68afaf66c785408d8d10fb2eaedab8db3012
xCr1s12 <CristobalAlexis.onate@alumnos.ulagos.cl>
2024-09-04 10:15:28 -0400

    asdasd

--- Bot/app.py
+++ Bot/app.py
@@ -11,7 +11,7 @@ my_secret = os.getenv("TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
 @bot.event
 async def on_ready():
